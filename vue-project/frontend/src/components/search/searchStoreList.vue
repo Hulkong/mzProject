34a861070fd2bf351b84f96a638f084ce46c972d
이다솜 <dasom2d@gmail.com>
2019-07-16 00:42:28 +0900

    마커 마우스오버시 마커위에 가게이름 노출

--- vue-project/frontend/src/components/search/searchStoreList.vue
+++ vue-project/frontend/src/components/search/searchStoreList.vue
@@ -8,17 +8,17 @@
                     <v-toolbar color="pink" dark>
                         <v-toolbar-title>{{searchWord}} 검색결과</v-toolbar-title>
                     </v-toolbar>
-                    <v-list two-line v-if="false">
+                    <v-list two-line>
                         <template v-for="(place, index) in searchPlaces">
-                        <v-list-tile :key="index" avatar ripple>
-                            <v-list-tile-content>
-                            <v-list-tile-title>{{ place.place_name }}</v-list-tile-title>
-                            <v-list-tile-sub-title class="text--primary">{{ place.phone }}</v-list-tile-sub-title>
-                            <v-list-tile-sub-title>{{ place.road_address_name }}</v-list-tile-sub-title>
+                        <v-list-tile :key="index" avatar ripple> 
+                            <v-list-tile-content  @mouseover="showListMarkerTooltip(place)" @mouseout="closeListMarkerTooltip" >
+                            <v-list-tile-title  v-html="place.place_name"></v-list-tile-title>
+                            <v-list-tile-sub-title v-html="place.phone" class="text--primary"></v-list-tile-sub-title>
+                            <v-list-tile-sub-title v-html="place.road_address_name"></v-list-tile-sub-title>
                             </v-list-tile-content>
                             <v-list-tile-action>
                             <v-list-tile-action-text>{{ place.distance }}</v-list-tile-action-text>
-                            <v-icon color="pink">fas fa-plus-circle</v-icon>
+                            <v-icon color="pink" @click="">fas fa-plus-circle</v-icon>
                             </v-list-tile-action>
                         </v-list-tile>
                         <v-divider v-if="index + 1 < searchPlaces.length" :key="`divider-${index}`"></v-divider>
@@ -53,6 +53,7 @@ export default {
         if (status === kakao.maps.services.Status.OK) {
           // 정상적으로 검색이 완료됐으면
           // 검색 목록과 마커를 표출합니다
+          this.searchPlaces = data;
           this.displayPlaces(data);
         } else if (status === kakao.maps.services.Status.ZERO_RESULT) {
             alert('검색 결과가 존재하지 않습니다.');
@@ -62,13 +63,29 @@ export default {
             return;
         }
 
-      },
+    },
+    showListMarkerTooltip(place){
+        let placePosition = new kakao.maps.LatLng(place.y, place.x);
+        let marker = new kakao.maps.Marker({
+            position: placePosition
+        });
+       this.mInfoWindo = new kakao.maps.InfoWindow({
+            content: place.place_name
+        });
+        let outer = makeOverListener(this.map, marker, this.mInfoWindo);
+        outer();
+    },
+    closeListMarkerTooltip(){
+    //    let infowindow = new kakao.maps.InfoWindow();
+        let outer = makeOutListener(this.mInfoWindo);
+        outer();
+    },
     displayPlaces(places) {
-        this.searchPlaces = places;
 
         let bounds = new kakao.maps.LatLngBounds();
         let fragment = document.createDocumentFragment();
 
+        this.removeMarker();
          for ( var i=0; i<places.length; i++ ) {
             
             // 마커를 생성하고 지도에 표시합니다
@@ -78,26 +95,29 @@ export default {
             // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
             // LatLngBounds 객체에 좌표를 추가합니다
             bounds.extend(placePosition);
-        //    this.prevealMarker(marker, places[i].place_name);
+            
+            let infowindow = new kakao.maps.InfoWindow({
+                content: places[i].place_name
+            });
 
-
-var iwContent = '<div style="padding:5px;">Hello World!</div>';
-
-// 인포윈도우를 생성합니다
-var infowindow = new kakao.maps.InfoWindow({
-    content : iwContent
-});
-
-// 마커에 클릭이벤트를 등록합니다
-kakao.maps.event.addListener(marker, 'click', function() {
-      // 마커 위에 인포윈도우를 표시합니다
-      infowindow.open(map, marker);  
-});
+            kakao.maps.event.addListener(marker, 'mouseover', makeOverListener(this.map, marker, infowindow));
+            kakao.maps.event.addListener(marker, 'mouseout', makeOutListener(infowindow));
 
         }
         this.map.setBounds(bounds); 
-      },
-    addMarker(position, idx, title) {
+    },
+    showStoreInfo(place){
+        let placePosition = new kakao.maps.LatLng(place.y, place.x);
+        let marker = new kakao.maps.Marker({
+            position: placePosition
+        });
+        let infowindow = new kakao.maps.InfoWindow({
+            content: place.place_name
+        });
+        makeOverListener(this.map, marker, infowindow);
+        makeOutListener(infowindow);
+    },
+    addMarker(position, idx) {
         var imageSrc = 'http://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_number_blue.png', // 마커 이미지 url, 스프라이트 이미지를 씁니다
             imageSize = new kakao.maps.Size(36, 37),  // 마커 이미지의 크기
             imgOptions =  {
@@ -116,48 +136,49 @@ kakao.maps.event.addListener(marker, 'click', function() {
 
         return marker;
     },
-    displayInfowindow(marker, title) {
-        var content = '<div style="padding:5px;z-index:1;">' + title + '</div>';
-
-        this.infowindowObject.setContent(content);
-        this.infowindowObject.open(map, marker);
-}
+    removeMarker() {
+        for ( var i = 0; i < this.markers.length; i++ ) {
+            this.markers[i].setMap(null);
+        }   
+        this.markers = [];
+    }
   },
   data () {
     return {
         map: '',
         psObject: new kakao.maps.services.Places(),
-        infowindowObject: new kakao.maps.InfoWindow({zIndex:1}),
         searchPlaces: [],
-        markers: []
+        markers: [],
+        mInfoWindow: null
     }
   }
 };
 
-function  prevealMarker(marker, title) {
-        kakao.maps.event.addListener(marker, 'mouseover', function() {
-                    console.log("d");
-                });
-
-                // kakao.maps.event.addListener(marker, 'mouseout', function() {
-                //     this.infowindowObject.close();
-                // });
-
-                // itemEl.onmouseover =  function () {
-                //     this.displayInfowindow(marker, title);
-                // };
-
-                // itemEl.onmouseout =  function () {
-                //     infowindow.close();
+function makeOverListener(map, marker, infowindow) {
+    return function() {
+        infowindow.open(map, marker);
     };
+}
+
+function makeOutListener(infowindow) {
+    return function() {
+        infowindow.close();
+    };
+}
+
 </script>
 
 
 <style>
 .searchStoreListApp{
-    position: fixed;
+    /* position: fixed;
     z-index: 101;
     top: 60px;
-    width: 1000px;
+    width: 1000px; */    
+    position: relative;
+    z-index: 101;
+    width: 800px;
+    top: 100px;
+    right: 200px;
 }
 </style>
